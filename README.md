a6b5387a913792ee9beb2c49aafa43de41b0c05a
joselopez59 <mail@joselopez.de>
2020-06-21 18:26:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-jvrbwb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-jvrbwb)
---
+++ app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { NgCalendarModule  } from 'ionic2-calendar';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    NgCalendarModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
